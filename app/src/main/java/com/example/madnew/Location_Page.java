7c7df3b2dd3610766ace828fc6e62060b89caddb
MAD <kumarathisara01@gmail.com>
2021-09-17 14:46:31 +0530

    hh

---
+++ app/src/main/java/com/example/madnew/Location_Page.java
@@ -0,0 +1,14 @@
+package com.example.madnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Location_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_page);
+    }
+}
