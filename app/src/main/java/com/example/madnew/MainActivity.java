e9be80eb0abe0498706c1e2c6bb51637f8441eeb
ManualCS <manulchandila2000@gmail.com>
2021-09-25 21:34:08 +0530

    Manul

--- app/src/main/java/com/example/madnew/MainActivity.java
+++ app/src/main/java/com/example/madnew/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
+        // getSupportActionBar().hide();
 
 
         login = findViewById(R.id.Login);
