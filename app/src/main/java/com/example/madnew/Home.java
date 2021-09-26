000f93cb9a9474297a7f9ffe5672d3da0edfc618
ManualCS <manulchandila2000@gmail.com>
2021-09-26 12:11:40 +0530

    Manul

--- app/src/main/java/com/example/madnew/Home.java
+++ app/src/main/java/com/example/madnew/Home.java
@@ -2,7 +2,11 @@ package com.example.madnew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ImageView rental = (ImageView) findViewById(R.id.rental);
+        rental.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentrent = new Intent(Home.this,Rental.class);
+                startActivity(intentrent);
+            }
+        });
     }
 }
+
+
+
+
+
---
+++ app/src/main/java/com/example/madnew/RCheckout.java
@@ -0,0 +1,14 @@
+package com.example.madnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RCheckout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rental_checkout);
+    }
+}
